f7169a2cf288063666c82cf176bac864d7327f29
jonpuppet <the_roof@hotmail.com>
2016-08-24 10:27:59 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,4 +44,12 @@ node default {
   #   class { 'my_class': }
   notify { "Hello World!, my name is ${::hostname}": }
   include examples::fundamentals
+  
+  file {'/etc/motd':
+  ensure => file,
+  owner =>'root',
+  group =>'root',
+  mode =>'0644',
+  source => 'This is a sample MOTD by Jon',
+  }
 }
