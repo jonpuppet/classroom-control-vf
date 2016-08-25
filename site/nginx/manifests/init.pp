2fdff7ab3352157ce22948654f3d6fccf93cbf7f
jonpuppet <the_roof@hotmail.com>
2016-08-25 11:09:00 -0600

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,36 +3,31 @@ class nginx {
     ensure => present,
   }
 
+  File {
+  owner => 'root',
+  group => 'root',
+  mode => '0664',
+  }
+
   file {'/var/www':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
     mode => '0755',
     }
   
   file {'index.html':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     path => '/var/www/index.html',
     source => 'puppet:///modules/nginx/index.html',
     }
     
   file {'nginx.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     path => '/etc/nginx/nginx.conf',
     source => 'puppet:///modules/nginx/nginx.conf',
     }
     
   file {'default.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     path => '/etc/nginx/conf.d/default.conf',
     source => 'puppet:///modules/nginx/default.conf',
   }
