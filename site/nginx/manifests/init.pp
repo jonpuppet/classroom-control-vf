6cadd0c7df98469afc66ac369789f6b54c305f83
jonpuppet <the_roof@hotmail.com>
2016-08-25 16:10:28 -0600

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -49,7 +49,7 @@ file { "${docroot}/index.html":
 
 file { "${confdir}/nginx.conf":
   ensure => file,
-  content => template('templates/nginx.conf.erb'),
+#  content => template('templates/nginx.conf.erb'),
   notify => Service['nginx'],
 }
 
