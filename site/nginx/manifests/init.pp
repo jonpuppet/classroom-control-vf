2df9810232ffa2436d1a145850d2f8ee0c941ba3
jonpuppet <the_roof@hotmail.com>
2016-08-25 10:26:32 -0600

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,46 @@
+class nginx {
+  package {'nginx':
+    ensure => present,
+  }
+
+  file {'/var/www':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+    }
+  
+  file {'index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    path => '/var/www/index.html',
+    source => 'puppet:///modules/nginx/index.html',
+    }
+    
+  file {'nginx.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    path => /etc/nginx/nginx.conf',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    }
+    
+  file {'default.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root'
+    mode => '0644',
+    path => '/etc/nginx/conf.d/default.conf',
+    source => 'puppet:///modules/nginx/default.conf',
+  }
+  
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => [File['nginx.conf'], File['default.conf']]
+    }
+    
+}
